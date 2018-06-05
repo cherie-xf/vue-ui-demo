f5149aaaa0a543f3e52e4adff570e1c2c7ebd47f
Cherie Fu <cheriefu@fortinet.com>
2018-06-05 11:11:17 -0700

    edit project config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,6 +11,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
 const env = require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
