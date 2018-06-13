186cbe6b2f9f8fb758ff8ee2f6efb31f1b2b1d07
Cherie Fu <cheriefu@fortinet.com>
2018-06-13 16:55:31 -0700

    add layout slider component

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -3,16 +3,7 @@
     <sidebar></sidebar>
     <navbar></navbar>
     <v-navigation-drawer temporary :value="leftdrawer" fixed></v-navigation-drawer>
-    <v-navigation-drawer temporary :value="rightdrawer" right fixed class="right-drawer">
-      <div class="user-avatar" @click.stop="toggleRightDrawer">
-          <v-badge overlap>
-            <span slot="badge">3</span>
-            <v-avatar color="purple red--after">
-              <img src="https://vuetifyjs.com/static/doc-images/john.jpg" alt="John">
-            </v-avatar>
-          </v-badge>
-      </div>
-    </v-navigation-drawer>
+    <slider></slider>
     <v-content>
         <v-jumbotron :gradient="gradient" :height="`100%`">
           <v-container fluid fill-height>
@@ -25,21 +16,19 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { Navbar, Sidebar, AppMain } from './components'
+import { Slider, Navbar, Sidebar, AppMain } from './components'
 import SvgIcon from '@/components/SvgIcon.vue'// svg组件
 
 export default {
   name:'Layout',
-  components: { Navbar,Sidebar,AppMain, 'svg-icon': SvgIcon },
+  components: { Slider, Navbar,Sidebar,AppMain,'svg-icon': SvgIcon },
   data:() => ({
     //gradient:'to top, #cfd9df 0%, #e2ebf0 100%',
     //gradient:'to top, #a8edea 0%, #fed6e3 100%',
   }),
   computed: {
     ...mapGetters([
-      'sidebar',
       'leftdrawer',
-      'rightdrawer',
       'gradient'
     ]),
     routes() {
@@ -47,28 +36,8 @@ export default {
     },
   },
   methods: {
-    toggleRightDrawer() {
-      this.$store.dispatch('ToggleRight')
-    },
   }
 }
 </script>
 <style lang="less" scoped>
-  .right-drawer{
-    overflow: initial;
-  }
-  .user-avatar {
-    cursor: pointer;
-    background-color: rgba(204, 204, 204, 0.8);
-    position: absolute;
-    top: 15px;
-    left: -100px;
-    width:100px;
-    height: 50px;
-    display: flex;
-    align-content: center;
-    border-bottom-left-radius: 30px; 
-    border-top-left-radius: 30px;
-  }
-
 </style>
---
+++ src/views/layout/components/Slider.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-navigation-drawer temporary :value="rightdrawer" right fixed class="right-drawer">
+      <div class="user-avatar" @click.stop="toggleRightDrawer">
+          <v-badge overlap>
+            <span slot="badge">3</span>
+            <v-avatar color="purple red--after">
+              <img src="https://vuetifyjs.com/static/doc-images/john.jpg" alt="John">
+            </v-avatar>
+          </v-badge>
+      </div>
+    </v-navigation-drawer>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'rightdrawer',
+    ]),
+  },
+  methods: {
+    toggleRightDrawer() {
+      this.$store.dispatch('ToggleRight')
+    },
+  }
+}
+</script>
+<style lang="less" scoped>
+  .right-drawer{
+    overflow: initial;
+  }
+  .user-avatar {
+    cursor: pointer;
+    background-color: rgba(204, 204, 204, 0.8);
+    position: absolute;
+    top: 15px;
+    left: -100px;
+    width:100px;
+    height: 50px;
+    display: flex;
+    align-content: center;
+    border-bottom-left-radius: 30px; 
+    border-top-left-radius: 30px;
+  }
+</style>
